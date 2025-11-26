e8885a62f3446da6f5b6ec4d240f889239977f37
Manjundhar <103714273+Manju025@users.noreply.github.com>
2025-11-26 19:12:49 +0530

    Refactor footer links to use anchor tags
    Updated email link to use anchor tag for better accessibility and added target and rel attributes.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,10 @@
-import { FaGoogle, FaInstagram, FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
+import {
+  FaGoogle,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 import "./index.css";
@@ -6,16 +12,28 @@ import "./index.css";
 const Footer = () => (
   <footer className="footer">
     <div className="footer-icons">
-      <Link to="manjundhar1701@gmail.com" className="icon-btn">
+      <a
+        href="mailto:manjundhar1701@gmail.com"
+        className="icon-btn"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FaGoogle />
-      </Link>
+      </a>
+
       <Link to="https://x.com/ManjundharM" className="icon-btn">
         <FaTwitter />
       </Link>
-      <Link to="https://www.instagram.com/_manju.smartz0_/#" className="icon-btn">
+      <Link
+        to="https://www.instagram.com/_manju.smartz0_/#"
+        className="icon-btn"
+      >
         <FaInstagram />
       </Link>
-      <Link to="https://www.linkedin.com/in/manjundhar-adagiri/"  className="icon-btn">
+      <Link
+        to="https://www.linkedin.com/in/manjundhar-adagiri/"
+        className="icon-btn"
+      >
         <FaLinkedin />
       </Link>
       <Link to="https://github.com/Manju025" className="icon-btn">
